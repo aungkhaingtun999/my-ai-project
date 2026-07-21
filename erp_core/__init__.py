dccb585d3b61b57f5a6ca308b7e0d8bc4860b47f
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-07-21 14:16:18 +0630

    Update __init__.py

--- erp_core/__init__.py
+++ erp_core/__init__.py
@@ -1,10 +1,13 @@
 # erp_core/__init__.py
 from .exceptions import *
 from .config import *
-from .context import ERPContext, CacheManager
+from .context import ERPContext, CacheManager, generate_tx_id, generate_transaction_id
 from .base_repo import db, get_connection, DatabaseHealth, database_health_check, money, money_float, validate_uuid, serialize_json, safe_execute
-from .repositories import RepositoryCoordinator, ProductRepository, WarehouseRepository, CustomerRepository, SupplierRepository, SalesRepository
 from .rpc_engine import RPCEngine
+from .repositories import (
+    RepositoryCoordinator, BaseRepository, ProductRepository, 
+    WarehouseRepository, CustomerRepository, SupplierRepository, SalesRepository
+)
 from .services import (
     AccountingLedgerService, CustomerService, SalesService, 
     InventoryService, PurchaseService, RefundService, 
@@ -13,4 +16,3 @@ from .services import (
     get_fifo_cogs, create_audit_log, require_login,
     get_warehouses, get_suppliers, get_customers, get_products
 )
-
