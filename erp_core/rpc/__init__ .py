94501f16fa1cd7b0106468a9b708d712b706e680
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-07-22 18:20:45 +0630

    Update __init__ .py

--- erp_core/rpc/__init__ .py
+++ erp_core/rpc/__init__ .py
@@ -3,6 +3,7 @@
 # ERP ENTERPRISE RPC PACKAGE
 # ==============================================================================
 
+from .engine import RPCEngine
 
 from .checkout_rpc import (
     checkout_sale_rpc
@@ -26,7 +27,7 @@ from .stock_rpc import (
 
 
 __all__ = [
-
+    "RPCEngine",
     "checkout_sale_rpc",
 
     "purchase_receive_rpc",
