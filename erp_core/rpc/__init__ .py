76b2bf194243ec07f73c1376c9b1bf4b28bfe3c6
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-07-22 23:12:43 +0630

    Simplify RPC imports in __init__.py
    Refactor RPC imports by removing try-except blocks and unused comments.

--- erp_core/rpc/__init__ .py
+++ erp_core/rpc/__init__ .py
@@ -1,84 +1,28 @@
 # ==============================================================================
 # erp_core/rpc/__init__.py
 # ERP ENTERPRISE RPC PACKAGE v30.11
+# SAFE EXPORT
 # ==============================================================================
 
 
-"""
-RPC Gateway Layer
-
-pages
- |
- erp_core
- |
- rpc
- |
- service
-"""
+from .checkout_rpc import (
+    checkout_sale_rpc
+)
 
 
-# ------------------------------------------------------------------------------
-# CHECKOUT
-# ------------------------------------------------------------------------------
-
-try:
-
-    from .checkout_rpc import (
-        checkout_sale_rpc
-    )
-
-except Exception as e:
-
-    checkout_sale_rpc = None
+from .purchase_rpc import (
+    purchase_receive_rpc
+)
 
 
-
-# ------------------------------------------------------------------------------
-# PURCHASE
-# ------------------------------------------------------------------------------
-
-try:
-
-    from .purchase_rpc import (
-        purchase_receive_rpc
-    )
-
-except Exception:
-
-    purchase_receive_rpc = None
+from .refund_rpc import (
+    refund_sale_rpc
+)
 
 
-
-# ------------------------------------------------------------------------------
-# REFUND
-# ------------------------------------------------------------------------------
-
-try:
-
-    from .refund_rpc import (
-        refund_sale_rpc
-    )
-
-except Exception:
-
-    refund_sale_rpc = None
-
-
-
-# ------------------------------------------------------------------------------
-# STOCK
-# ------------------------------------------------------------------------------
-
-try:
-
-    from .stock_rpc import (
-        stock_adjustment_rpc
-    )
-
-except Exception:
-
-    stock_adjustment_rpc = None
-
+from .stock_rpc import (
+    stock_adjustment_rpc
+)
 
 
 
