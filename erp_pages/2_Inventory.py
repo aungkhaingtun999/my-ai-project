ffab7b4f8865905c689d8dd5b52ff2b50be488bf
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-22 13:17:24 +0630

    Add files via upload

---
+++ erp_pages/2_Inventory.py
@@ -0,0 +1,7 @@
+# ==============================================================================
+# erp_pages/2_Inventory.py
+# ERP ENTERPRISE INVENTORY ENTRY
+# ==============================================================================
+
+import streamlit as st
+from erp_pages.inventory.page import run
