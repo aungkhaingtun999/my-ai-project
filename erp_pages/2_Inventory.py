7da125792f51077704d2f6ad8dc9a90d4872414b
aungkhaingtun999 <aungkhaingtun.agdb@gmail.com>
2026-08-22 13:16:53 +0630

    Delete erp_pages/2_Inventory.py

--- erp_pages/2_Inventory.py
+++
@@ -1,7 +0,0 @@
-# ==============================================================================
-# erp_pages/2_Inventory.py
-# ERP ENTERPRISE INVENTORY ENTRY
-# ==============================================================================
-
-import streamlit as st
-from erp_pages.inventory.page import run
